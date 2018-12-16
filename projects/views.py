6a28dd6d335ce06828eefb95fe1f05af83647d26
fr3kz <tektmap1@gmail.com>
2018-12-16 10:12:04 +0200

    collect static/ new todos

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-# TODO create index & create function for posting projects
+# TODO create index & create function for posting projects 
+#TODO create view for particular project & view for your project (details etc.)
+def index(request):
+    return
+
 def create(request):
     return 
